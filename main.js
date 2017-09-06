d7c666fe344c54c9a756f66ada6310d80d355d3a
Garrett Koller <gkoller@catalist.us>
2017-09-06 17:28:18 -0400

    WIP: Send play request to Haystack TV app on Chromecast

--- main.js
+++ main.js
@@ -33,11 +33,11 @@ function launchYouTube(connectedDevice) {
         connectedDevice.application('YouTube', (appErr, appObj) => {
           if (!appErr) {
             console.log('YouTube application', util.inspect(appObj));
-            appObj.run('urn:x-cast:com.google.cast.demo.tictactoe', (sessionErr, sessionObj) => {
-              if (!sessionErr) {
+            appObj.run('urn:x-cast:com.google.cast.demo.tictactoe', (sessionJoinErr, sessionObj) => {
+              if (!sessionJoinErr) {
                 console.log('Got a YouTube session', util.inspect(sessionObj));
               } else {
-                console.log('Unable to create a YouTube session', sessionErr);
+                console.log('Unable to create a YouTube session', sessionJoinErr);
               }
             });
           } else {
@@ -61,20 +61,23 @@ function launchHaystack(connectedDevice) {
         connectedDevice.application('898AF734', (appErr, appObj) => {
           if (!appErr) {
             console.log('Haystack application', util.inspect(appObj));
-            //TODO: appObj.run or appObj.join
             // appObj.join('urn:x-cast:com.google.cast.demo.tictactoe', ...)
-            appObj.join('urn:x-cast:com.haystack.android', (sessionErr, sessionObj) => {
-              if (!sessionErr) {
-                console.log('Joined a Haystack session', util.inspect(sessionObj));
-                sessionObj.send({ data: 'hello' }, (messageErr, messageResponse) => {
-                  if (!messageErr) {
-                    console.log('Got an answer!', util.inspect(messageResponse));
+            // join session if one already exists
+            //TODO: use 'urn:x-cast:com.haystack.android' or 'urn:x-cast:com.google.cast.media'?
+            appObj.join('urn:x-cast:com.google.cast.media', (sessionJoinErr, sessionJoinObj) => {
+              if (!sessionJoinErr) {
+                console.log('Joined a Haystack session', util.inspect(sessionJoinObj));
+                sendHaystackPlay(sessionJoinObj);
+              } else {
+                // fallback: run session instead of join
+                appObj.run('urn:x-cast:com.google.cast.media', (sessionRunErr, sessionRunObj) => {
+                  if (!sessionRunErr) {
+                    console.log('Created a Haystack session', util.inspect(sessionRunObj));
+                    sendHaystackPlay(sessionRunObj);
                   } else {
-                    console.log('Unable to send message:', messageErr);
+                    console.log('Unable to join or create a Haystack session', sessionRunErr);
                   }
                 });
-              } else {
-                console.log('Unable to join a Haystack session', sessionErr);
               }
             });
           } else {
@@ -84,6 +87,24 @@ function launchHaystack(connectedDevice) {
       } else {
         console.log('Chomecast is not idle, so will not launch Haystack app');
       }
+    }
+  });
+}
+
+function sendHaystackPlay(sessionObj) {
+  var playMessage = {
+    type: "message",
+    Aa: !1,
+    defaultPrevented: !1,
+    bd: !0,
+    senderId: "7d2430ae-db4d-efbd-b250-b11d491f9832.249:sender-0",
+    data: '{"currentTime": 0, "customData": {"platform": "google cast"}, "requestId": 5, "type": "GET_STATUS", "autoplay": true}'
+  };
+  sessionObj.send(playMessage, (messageErr, messageResponse) => {
+    if (!messageErr) {
+      console.log('Got an answer!', util.inspect(messageResponse));
+    } else {
+      console.log('Unable to send message:', messageErr);
     }
   });
 }
